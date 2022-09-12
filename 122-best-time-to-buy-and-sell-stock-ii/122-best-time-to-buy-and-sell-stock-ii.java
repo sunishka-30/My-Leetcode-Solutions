531f983c79ea14d24c83ce2a34fa3dc318de0b7d
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-09-12 10:34:44 +0530

    Time: 4 ms (15.35%), Space: 44.7 MB (28.36%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,36 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
-        for(int i=0 ;i<prices.length ;i++){
-            for(int j=0; j<2;j++){
-                
-                    dp[i][j]=-1;
-                
-            }
-        }
-        return maxp(prices , 0 , 1 ,dp);
-    }
-    
-    public int maxp(int[] prices , int current , int canbuy ,int[][] dp){
+        int n  = prices.length;
+        int profit = 0;
         
-        if(current>=prices.length){
-            return 0;
+        for(int i=1;i<n;i++)
+        {
+            if(prices[i]>prices[i-1])
+                profit+=(prices[i]-prices[i-1]);
         }
-        if(dp[current][canbuy]!=-1){
-            return dp[current][canbuy];
-        }
-        int idle = maxp(prices ,current+1 , canbuy , dp);
-        int buy=0;
-        int sell=0;
-        if(canbuy==1){
-            buy= -prices[current]+maxp(prices, current+1 , 0 ,dp);
-        }
-        if(canbuy==0){
-            sell= prices[current]+maxp(prices ,current+1 , 1,dp);
-        }
-        dp[current][canbuy]=Math.max(idle , Math.max(buy ,sell));
-        return Math.max(idle , Math.max(buy ,sell));
-
         
+        return profit;
     }
 }
