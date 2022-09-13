605faaea024924a01c475323f358de651b83957d
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-09-13 11:24:44 +0530

    Time: 1 ms (89.82%), Space: 42.8 MB (59.93%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long tempP = n;
+        if(tempP<0)
+          tempP = -1*tempP;
+        
+        while(tempP>0)
+        {
+            if(tempP%2==0)
+            {
+                x=x*x;
+                tempP=tempP/2;
+            }
+            else
+            {
+                ans=ans*x;
+                tempP=tempP-1;
+            }
+        }
+        
+        if(n<0)
+            return (double)1.0/(double)ans;
+        return ans;
+        
+    }
+}
