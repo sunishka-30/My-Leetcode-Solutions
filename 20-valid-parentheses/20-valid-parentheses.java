ae035059d510b94c950196267e2e2455ae4be420
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-09-13 16:14:17 +0530

    Time: 3 ms (63.67%), Space: 42 MB (51.91%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char a = s.charAt(i);
+            if(a=='(' || a=='{' || a=='[')
+            {
+                st.push(a);
+            }
+            else if(st.isEmpty())
+                return false;
+            else if(a==')' && st.pop()!='(') return false;
+            else if(a==']' && st.pop()!='[') return false;
+            else if(a=='}' && st.pop()!='{') return false;
+            
+        }
+        
+        return st.isEmpty();
+    }
+}
