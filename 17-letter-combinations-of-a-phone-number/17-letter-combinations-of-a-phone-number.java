aa8d733681a32ea2db19f8bf9973d83d1c9c7b9e
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-09-14 15:49:38 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (97.28%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -18,14 +18,14 @@ class Solution {
     }
 
     void dfs(String digits, List<String> ans, HashMap<Integer,String> mp, StringBuilder s, int start) {
-        if (start == digits.length()) {
+        if (start == digits.length()) { // when s = ad and so on value of start becomes equals to length of digits string
             ans.add(s.toString());
         } else {
-            String temp = mp.get(digits.charAt(start) - '0');
+            String temp = mp.get(digits.charAt(start) - '0');    // for "23", first temp = abc then a is appended to string s after that temp becomes def thus further appending d with a and do on. 
             for (int i = 0; i < temp.length(); i++) {
-                s.append(temp.charAt(i));
+                s.append(temp.charAt(i));   //add a to string s than d after that remove d and add e than remove e add f thus making ad, ae, af and further more like this 
                 dfs(digits, ans, mp, s, start + 1);
-                s.deleteCharAt(s.length() - 1);
+                s.deleteCharAt(s.length() - 1); // to remove for making combinations of a with each letter of other string 
             }
         }
     }
