1ba8fea47ba3a5f636aa9fd79955ca717076da2e
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-09-15 15:48:27 +0530

    Time: 197 ms (42.40%), Space: 41.7 MB (83.59%) - LeetHub

---
+++ 79-word-search/79-word-search.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean exist(char[][] mat, String target) {
+        boolean []ans=new boolean[1];
+        ans[0]=false;
+        for(int i=0;i<mat.length;i++)
+        {
+            for(int j=0;j<mat[0].length;j++)
+            {
+               dfs(mat,i,j,0,target,ans);
+            }
+        }
+        
+        return ans[0];
+    }
+    static void dfs(char [][]mat, int i, int j, int ind, String target, boolean[] ans)
+    {
+        if(i<0 || j<0 || i==mat.length || j==mat[0].length || ind>=target.length() || target.charAt(ind)!=mat[i][j])
+        return ;
+        
+        if(ind==target.length()-1)
+        {
+            ans[0]=true;   
+            return ;
+        }
+        
+        char ch = mat[i][j];
+        mat[i][j]='0';
+        
+        dfs(mat,i+1,j,ind+1,target,ans);
+        dfs(mat,i-1,j,ind+1,target,ans);
+        dfs(mat,i,j+1,ind+1,target,ans);
+        dfs(mat,i,j-1,ind+1,target,ans);
+        
+        mat[i][j]=ch;
+        
+        
+    }
+}
