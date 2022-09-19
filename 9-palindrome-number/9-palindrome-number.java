d0e0e6ad494a0ab20bfbc082991ce46c289191f5
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-09-19 14:25:48 +0530

    Time: 14 ms (65.78%), Space: 44.5 MB (58.09%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+        int res =0;
+        int temp=x;
+        while(x!=0){
+            int rem = x%10;
+            res = res*10 + rem;
+            x=x/10;
+        }
+        return temp==res;
+    }
+}
