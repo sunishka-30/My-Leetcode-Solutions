0f199d048b3786003adf70018cc169aff8323725
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-09-24 22:00:45 +0530

    Time: 3 ms (49.09%), Space: 41.1 MB (59.55%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -7,14 +7,16 @@ class Solution {
             n=0;
             while(temp>0)
             {
-                n=n+(int)Math.pow((temp%10),2);
+                n = n + (int)Math.pow(temp%10,2);
                 temp=temp/10;
             }
+            
             if(l.contains(n))
                 return false;
             else
                 l.add(n);
         }
+        
         return true;
     }
 }
