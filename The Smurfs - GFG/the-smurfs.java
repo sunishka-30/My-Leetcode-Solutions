3995e5ef6b25c5714977d61f83a4cb65c9ec7efe
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-10-26 11:37:39 +0530

    Added solution - LeetHub

---
+++ The Smurfs - GFG/the-smurfs.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int n = Integer.parseInt(in.readLine());
+            String a[] = in.readLine().trim().split("\\s+");
+            
+            Solution ob = new Solution();
+            System.out.println(ob.minFind(n, a));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int minFind(int n, String a[]){
+        // code here 
+        int r=0,b=0,g=0;
+
+       for(int i=0;i<n;i++){
+
+           if(a[i].equals("R"))
+
+           r++;
+
+           else if(a[i].equals("B"))
+
+           b++;
+
+           else g++;
+
+       }
+       
+       
+
+       if((r%2==0&&g%2==0&&b%2==0)||(r%2==1&&g%2==1&&b%2==1))
+
+       return 2;
+
+       else return 1;
+    }
+}
