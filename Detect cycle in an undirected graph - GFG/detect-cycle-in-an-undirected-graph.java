8ded59bc642ff2ac9fbb4c79afff25af5e3e990d
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-11-08 22:14:21 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -31,62 +31,105 @@ class GFG {
 }
 // } Driver Code Ends
 
-class Pair{
-    int node;
-    int parent;
-    public Pair(int node, int parent)
-    {
-        this.node=node;
-        this.parent=parent;
-    }
-}
+// class Pair{
+//     int node;
+//     int parent;
+//     public Pair(int node, int parent)
+//     {
+//         this.node=node;
+//         this.parent=parent;
+//     }
+// }
 class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        // Code here
         
-        boolean[]vis = new boolean[V];
-        for(int i=0;i<V;i++)
-        vis[i]=false;
+        //BFS
         
+        // boolean[]vis = new boolean[V];
+        // for(int i=0;i<V;i++)
+        // vis[i]=false;
+        
+        // for(int i=0;i<V;i++)
+        // {
+        //     if(vis[i]==false)
+        //     {
+        //         if(bfs(i,adj,vis)==true)
+        //         return true;
+        //     }
+        // }
+        // return false;
+        
+        //DFS
+        
+        int[]vis = new int[V];
+       
         for(int i=0;i<V;i++)
         {
-            if(vis[i]==false)
+            if(vis[i]==0)
             {
-                if(bfs(i,adj,vis)==true)
+                if(dfs(i,-1,adj,vis)==true)
                 return true;
+                
             }
         }
+        
         return false;
+        
+        
+        
+        
         
     }
     
-    static boolean bfs(int src, ArrayList<ArrayList<Integer>> adj, boolean[]vis)
-    {
-        vis[src]=true;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(src,-1));
+    //BFS
+    
+    // static boolean bfs(int src, ArrayList<ArrayList<Integer>> adj, boolean[]vis)
+    // {
+    //     vis[src]=true;
+    //     Queue<Pair> q = new LinkedList<>();
+    //     q.add(new Pair(src,-1));
         
-        while(!q.isEmpty())
-        {
-            int node = q.peek().node;
-            int parent = q.peek().parent;
-            q.remove();
+    //     while(!q.isEmpty())
+    //     {
+    //         int node = q.peek().node;
+    //         int parent = q.peek().parent;
+    //         q.remove();
             
-            for(int it: adj.get(node))
-            {
-                if(vis[it]==false)
-                {
-                    vis[it]=true;
-                    q.add(new Pair(it,node));
-                }
-                else if(parent!=it)
-                {
-                    return true;
-                }
-            }
+    //         for(int it: adj.get(node))
+    //         {
+    //             if(vis[it]==false)
+    //             {
+    //                 vis[it]=true;
+    //                 q.add(new Pair(it,node));
+    //             }
+    //             else if(parent!=it)
+    //             {
+    //                 return true;
+    //             }
+    //         }
         
+    //     }
+    //     return false;
+    // }
+    
+    //DFS
+    
+    static boolean dfs(int node, int parent,ArrayList<ArrayList<Integer>> adj, int[]vis )
+    {
+        vis[node]=1;
+        for(int it:adj.get(node))
+        {
+            if(vis[it]==0)
+            {
+                if(dfs(it, node, adj, vis)==true)
+                return true;
+            }
+                else if(it!=parent)
+                return true;
+            
         }
+        
         return false;
     }
 }
