edeed2806cfe408f8f4e998749a0b8cf2d5a7f3c
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-11-22 19:13:55 +0530

    Added solution - LeetHub

---
+++ Magic Triplets - GFG/magic-triplets.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine().trim();
+            String[] S = s.split(" ");
+            int[] nums = new int[n];
+            for(int i = 0; i < n; i++){
+                nums[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int ans = ob.countTriplets(nums);
+            System.out.println(ans);         
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public int countTriplets(int[] nums){
+        // code here
+        int n = nums.length;
+        
+        if(n <= 2){
+            return 0;
+        }
+        int count = 0;
+        for(int i = 1;i<n-1;i++){
+          
+            int small = 0,large = 0;
+            for(int j = 0;j<i;j++){
+                if(nums[j] < nums[i]){
+                    small++;
+                }
+            }
+           
+            for(int k=i+1;k<n;k++){
+                if(nums[k] > nums[i]){
+                    large++;
+                }
+            }
+            
+            count += small*large;
+        }
+        return count;
+    }
+}
