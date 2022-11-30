12858255b0329eb9ce29af309a55e868df3377f5
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-11-30 11:57:47 +0530

    Time: 1 ms (88.28%), Space: 48 MB (46.98%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,27 +1,31 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n=image.length;
+        int m=image[0].length;
+        
         int[][]ans = image;
-        int iniColor = image[sr][sc];
+        int iniColor =  image[sr][sc];
+        dfs(image,ans,iniColor,color,sr,sc);
+        return image;
+    }
+    void dfs(int[][]image, int[][]ans, int iniColor, int color, int row, int col)
+    {
+        ans[row][col]=color;
+        int n=image.length;
+        int m=image[0].length;
         int[]delR = {-1,0,1,0};
         int[]delC = {0,1,0,-1};
-        dfs(sr,sc,ans,image,iniColor,color,delR,delC);
-        return ans;
-    }
-    
-    void dfs(int row, int col, int[][]ans,int[][]image,int iniColor,int color, int[] delR, int[] delC )
-    {
-        int n = image.length;
-        int m = image[0].length;
-        ans[row][col]=color;
         for(int i=0;i<4;i++)
         {
-            int nrow = row+delR[i];
-            int ncol = col+delC[i];
+        
+                int nrow = row+delR[i];
+                int ncol = col+delC[i];
+                if( nrow>=0 && nrow<n && ncol>=0 && ncol<m && ans[nrow][ncol]!=color && image[nrow][ncol]==iniColor)
+                {
+                    
+                    dfs(image,ans,iniColor,color,nrow,ncol);
+                }
             
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && ans[nrow][ncol]!=color && image[nrow][ncol]==iniColor)
-            {
-                dfs(nrow,ncol,ans,image,iniColor,color,delR,delC);
-            }
         }
     }
 }
