bb95b8aa7fe0a0ec8e4ee8557224e281d54f1607
Sunishka Sharma <73639308+sunishka-30@users.noreply.github.com>
2022-11-30 23:02:33 +0530

    Time: 19 ms (14.57%), Space: 55.4 MB (77.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,58 +3,55 @@ class Pair{
     int second;
     public Pair(int first, int second)
     {
-        this.first = first;
-        this.second = second;
+        this.first=first;
+        this.second=second;
     }
 }
 class Solution {
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        int n=grid.length;
+        int m=grid[0].length;
         int cnt=0;
-        int[][]vis = new int[n][m];
+        int[][]vis=new int[n][m];
         for(int i=0;i<n;i++)
+        {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]=='1' && vis[i][j]==0)
                 {
                     cnt++;
-                    bfs(i,j,grid,vis);
+                    bfs(grid,vis,i,j);
                 }
             }
+        }
         return cnt;
     }
-    
-    void bfs(int ro, int co, char[][]grid, int[][]vis)
+    void bfs(char[][]grid, int[][]vis, int r, int c)
     {
-        int n = grid.length;
-        int m = grid[0].length;
-        vis[ro][co]=1;
+        vis[r][c]=1;
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(ro,co));
+        q.add(new Pair(r,c));
+        int n=grid.length;
+        int m=grid[0].length;
+        int[]delR={-1,0,1,0};
+        int[]delC={0,1,0,-1};
         
         while(!q.isEmpty())
         {
             int row = q.peek().first;
             int col = q.peek().second;
-            
             q.remove();
-            
-            for(int delR = -1; delR<=1;delR++)
+            for(int i=0;i<4;i++)
+           {
+            int nrow=row+delR[i];
+            int ncol=col+delC[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&grid[nrow][ncol]=='1' && vis[nrow][ncol]==0)
             {
-                for(int delC = -1;delC<=1;delC++)
-                {
-                     if ((delR == -1 && delC == -1) || (delR == 1 && delC == -1) || (delR == -1 && delC == 1) || (delR == 1 && delC == 1)) continue;
-                    int nrow = row+delR;
-                    int ncol = col+delC;
-                    
-                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && vis[nrow][ncol]==0)
-                    {
-                        vis[nrow][ncol]=1;
-                        q.add(new Pair(nrow,ncol));
-                    }
-                }
-            }
+                q.add(new Pair(nrow,ncol));
+                vis[nrow][ncol]=1;
+             }
+           }
         }
+        
     }
 }
